40d731a6b8c86ce930de6d7d1c86ad201ba4e822
supuntharindu123 <supuntharindu1125@gmail.com>
2025-02-13 21:02:43 +0530

    Fix build and configuration

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".", // Ensure Vite looks in the correct directory
+  build: {
+    outDir: "dist", // Set the output directory to 'dist'
+  },
 });
